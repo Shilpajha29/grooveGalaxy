fb7a389c7df64be448e9aa77f738bb992715f138
Shilpa Jha <shilpajmd123@gmail.com>
2024-04-23 17:57:55 +0530

    fixing stuffs

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -16,14 +16,14 @@ function page() {
                 translateZ= "50"
                 className="text-xl font-bold text-neutral-600 dark:text-white"
               >
-                {course.title}
+                key ={course.title}
               </CardItem>
               <CardItem
                 as="p"
                 translateZ="60"
                 className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
               >
-                {course.description}
+                key ={course.description}
               </CardItem>
               <CardItem translateZ="100" className="w-full mt-4">
                 <Image
